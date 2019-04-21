4f37e91fd3d787d0733ded21e27da4632abf3cba
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-04-21 18:31:29 -0500

    make the ball bounce off the sides

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-let x = canvas.width/2;
-let y = canvas.height-30;
+const { height, width } = canvas; 
+
+let x = width / 2;
+let y = height - 30;
 let dx = 2;
 let dy = -2;
 
+let ballRadius = 10;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, width, height);
     drawBall();
+
+    const xPos = x + dx
+    const yPos = y + dy
+
+    if (xPos > width - ballRadius || xPos < ballRadius) {
+      dx = -dx;
+    }
+
+    if (yPos > height - ballRadius || yPos < ballRadius) {
+      dy = -dy;
+    }
+
     x += dx;
     y += dy;
 
