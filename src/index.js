91a8e4f2d7b234d92c3a41b94e32912a4b9dda33
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-04-22 09:33:30 -0500

    add paddle and move it using arrow keys

--- src/index.js
+++ src/index.js
@@ -8,7 +8,17 @@ let y = height - 30;
 let dx = 2;
 let dy = -2;
 
+// ball values
 let ballRadius = 10;
+
+// paddle values
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (width - paddleWidth) / 2;
+
+// paddle controls
+let rightPressed = false;
+let leftPressed = false;
 
 function drawBall() {
     ctx.beginPath();
@@ -18,10 +28,20 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, width, height);
     drawBall();
+    drawPaddle();
 
+    // ball collision detection
     const xPos = x + dx
     const yPos = y + dy
 
@@ -33,10 +53,37 @@ function draw() {
       dy = -dy;
     }
 
+    // paddle movement
+    if (rightPressed && paddleX < width - paddleWidth) {
+      paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+
+    // ball movement
     x += dx;
     y += dy;
 
     requestAnimationFrame(draw);
+}
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
 }
 
 draw();
