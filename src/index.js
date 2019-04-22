7c99c766fff50e6c965f282e4b0389b4f31f7a06
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-04-22 12:25:00 -0500

    add collision between paddle and ball, and game over condition

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-const { height, width } = canvas; 
+const { height, width } = canvas;
 
+// game values
+let gameOver = false;
+
+// ball values
 let x = width / 2;
 let y = height - 30;
 let dx = 2;
 let dy = -2;
-
-// ball values
 let ballRadius = 10;
 
 // paddle values
@@ -21,11 +23,11 @@ let rightPressed = false;
 let leftPressed = false;
 
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
 }
 
 function drawPaddle() {
@@ -37,34 +39,46 @@ function drawPaddle() {
 }
 
 function draw() {
-    ctx.clearRect(0, 0, width, height);
-    drawBall();
-    drawPaddle();
+  ctx.clearRect(0, 0, width, height);
+  drawBall();
+  drawPaddle();
 
-    // ball collision detection
-    const xPos = x + dx
-    const yPos = y + dy
+  // ball collision detection
+  const xPos = x + dx;
+  const yPos = y + dy;
 
-    if (xPos > width - ballRadius || xPos < ballRadius) {
-      dx = -dx;
-    }
+  if (xPos > width - ballRadius || xPos < ballRadius) {
+    dx = -dx;
+  }
 
-    if (yPos > height - ballRadius || yPos < ballRadius) {
+  if (yPos < ballRadius) {
+    dy = -dy;
+  } else if (yPos > height - ballRadius) {
+    // if paddle is there, bounce the ball as normal
+    if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
+    } else {
+      // if ball hits the bottom wall, end the game
+      gameOver = true;
     }
+  }
 
-    // paddle movement
-    if (rightPressed && paddleX < width - paddleWidth) {
-      paddleX += 7;
-    } else if (leftPressed && paddleX > 0) {
-      paddleX -= 7;
-    }
+  // paddle movement
+  if (rightPressed && paddleX < width - paddleWidth) {
+    paddleX += 7;
+  } else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
 
-    // ball movement
-    x += dx;
-    y += dy;
+  // ball movement
+  x += dx;
+  y += dy;
 
-    requestAnimationFrame(draw);
+  if (gameOver) {
+    alert('Game over');
+    return;
+  }
+  requestAnimationFrame(draw);
 }
 
 document.addEventListener('keydown', keyDownHandler, false);
